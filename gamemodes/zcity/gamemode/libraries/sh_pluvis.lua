ee30d0f53b4ac165858116a40492a16804ffef40
informal1337 <elisey.arhipov0402@gmail.com>
2026-05-23 13:38:23 +0300

    4

---
+++ gamemodes/zcity/gamemode/libraries/sh_pluvis.lua
@@ -0,0 +1,21 @@
+if CLIENT then
+	local fade = 0
+
+	net.Receive("FadeScreen", function()
+		fade = 0
+		timer.Simple(6, function()
+			hook.Add("RenderScreenspaceEffects", "ZB_ScreenFade", function()
+				surface.SetDrawColor(0, 0, 0, 255 * fade)
+				surface.DrawRect(-1, -1, ScrW() + 1, ScrH() + 1)
+				fade = Lerp(FrameTime() * 10, fade, 2)
+			end)
+			timer.Simple(2, function()
+				zb.RemoveFade()
+			end)
+		end)
+	end)
+
+	function zb.RemoveFade()
+		hook.Remove("RenderScreenspaceEffects", "ZB_ScreenFade")
+	end
+end
