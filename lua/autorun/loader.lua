24951610721ecc0597fb9f0263653e934d5e9c37
informal1337 <elisey.arhipov0402@gmail.com>
2026-05-31 21:14:51 +0300

    правки приколы кайы

--- lua/autorun/loader.lua
+++ lua/autorun/loader.lua
@@ -1,6 +1,6 @@
 hg = hg or {}
-hg.Version = "Beta 3.7"
-hg.GitHub_ReposOwner = "Rastawontfix, Sildom_Landor, informal1337"
+hg.Version = "Beta 4.0"
+hg.GitHub_ReposOwner = "informal1337, Rastawontfix, Sildom_Landor"
 hg.GitHub_ReposName = "Meleecity: Delicacy Reworked" 
 -- А силдом гей
 local print = print
