c3977a64b0fb2b75be4967b00b322731f35ff531
informal1337 <elisey.arhipov0402@gmail.com>
2026-05-31 22:40:30 +0300

    фикус

--- lua/homigrad/2_client/cl_tpik.lua
+++ lua/homigrad/2_client/cl_tpik.lua
@@ -985,10 +985,12 @@ function hg.DoTPIK(ply, ent)
     ply.segmentsr = ply.segmentsr or {}
     ply.segmentsr[1] = ply.segmentsr[1] or {Pos = Vector(), Len = 0}
     ply.segmentsr[2] = ply.segmentsr[2] or {Pos = Vector(), Len = 0}
+    ply.segmentsr[3] = ply.segmentsr[3] or {Pos = ply_r_hand_matrix:GetTranslation(), Len = limblength}
 
     ply.segmentsl = ply.segmentsl or {}
     ply.segmentsl[1] = ply.segmentsl[1] or {Pos = Vector(), Len = 0}
     ply.segmentsl[2] = ply.segmentsl[2] or {Pos = Vector(), Len = 0}
+    ply.segmentsl[3] = ply.segmentsl[3] or {Pos = ply_l_hand_matrix:GetTranslation(), Len = limblength}
     
     if not ply.BonesLength then
         ply.BonesLength = {}
@@ -1068,6 +1070,8 @@ function hg.DoTPIK(ply, ent)
 
             ply.segmentsr = segments
         end
+
+        segments[3] = segments[3] or {Pos = ply_r_hand_matrix:GetTranslation(), Len = limblength}
 
         local new = -(-segments[3].Pos)
 
@@ -1204,6 +1208,8 @@ function hg.DoTPIK(ply, ent)
             
             ply.segmentsl = segments
         end
+
+        segments[3] = segments[3] or {Pos = ply_l_hand_matrix:GetTranslation(), Len = limblength}
 
         local new = -(-segments[3].Pos)
 
