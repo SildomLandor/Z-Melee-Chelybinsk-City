422821119ccb773d86cf333d0d3bed64ef632d0f
informal1337 <elisey.arhipov0402@gmail.com>
2026-06-02 21:49:15 +0300

    123

--- lua/autorun/loader.lua
+++ lua/autorun/loader.lua
@@ -1,5 +1,5 @@
 hg = hg or {}
-hg.Version = "Beta 4.8"
+hg.Version = "Beta 5.1"
 hg.GitHub_ReposOwner = "informal1337, Rastawontfix, Sildom_Landor"
 hg.GitHub_ReposName = "Meleecity: Delicacy Reworked" 
 -- А силдом гей
