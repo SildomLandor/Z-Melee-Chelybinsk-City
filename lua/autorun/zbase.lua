e4eedc39bc5ff59742c2784af4a796ce308867a5
travasoco <81slam41k8@gmail.com>
2026-06-03 02:07:03 +0500

    Update zbase.lua

--- lua/autorun/zbase.lua
+++ lua/autorun/zbase.lua
@@ -1,47 +1,5 @@
 ZBaseInstalled = true
 
---[[=========================== CONV MESSAGE START ===========================]]--
-MissingConvMsg2 = CLIENT && function()
-
-    Derma_Query(
-        "This server does not have Zippy's Library installed, addons will function incorrectly!",
-
-        "ZIPPY'S LIBRARY MISSING!",
-        
-        "Get Zippy's Library",
-
-        function()
-            gui.OpenURL("https://steamcommunity.com/sharedfiles/filedetails/?id=3146473253")
-        end,
-
-        "Close"
-    )
-
-end || nil
-
-hook.Add("PlayerInitialSpawn", "MissingConvMsg2", function( ply )
-
-    if file.Exists("autorun/conv.lua", "LUA") then return end
-
-    local sendstr = 'MissingConvMsg2()'
-    ply:SendLua(sendstr)
-
-end)
---[[============================ CONV MESSAGE END ============================]]--
-
-if SERVER then
-    ZBaseDidConsoleLogo = ZBaseDidConsoleLogo
-    || MsgN("-- ███████╗██████╗░░█████╗░░██████╗███████╗ --")
-    || MsgN("-- ╚════██║██╔══██╗██╔══██╗██╔════╝██╔════╝ --")
-    || MsgN("-- ░░███╔═╝██████╦╝███████║╚█████╗░█████╗░░ --")
-    || MsgN("-- ██╔══╝░░██╔══██╗██╔══██║░╚═══██╗██╔══╝░░ --")
-    || MsgN("-- ███████╗██████╦╝██║░░██║██████╔╝███████╗ --")
-    || MsgN("-- ╚══════╝╚═════╝░╚═╝░░╚═╝╚═════╝░╚══════╝ --")
-    || MsgN("                                     -- █▀▀▄ █──█ 　 ▀▀█ ─▀─ █▀▀█ █▀▀█ █──█ --")
-    || MsgN("                                     -- █▀▀▄ █▄▄█ 　 ▄▀─ ▀█▀ █──█ █──█ █▄▄█ --")
-    || MsgN("                                     -- ▀▀▀─ ▄▄▄█ 　 ▀▀▀ ▀▀▀ █▀▀▀ █▀▀▀ ▄▄▄█ --")
-    || true
-end
 
 --[[
 ======================================================================================================================================================
